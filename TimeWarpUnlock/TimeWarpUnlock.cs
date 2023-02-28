3b165fff136435c1d07bd9e524a07597976a9d31
Will Cobb <willlcobb@gmail.com>
2023-02-27 22:40:03 -0600

    Initial Commit

---
+++ TimeWarpUnlock/TimeWarpUnlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BepInEx;
+using I2.Loc;
+using KSP.Game;
+using UnityEngine;
+using HarmonyLib;
+
+[BepInPlugin("TimeWarpUnlock", "Time Warp Unlock", "1.0")]
+
+public class TimeWarpUnlock : BaseUnityPlugin
+{
+    private void Start()
+    {
+        var harmony = new Harmony("maxratepatch");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(KSP.Sim.impl.TimeWarp))]
+[HarmonyPatch("GetMaxRateIndex")]
+class TimeWarpPatch
+{
+    static void Postfix(ref int __result)
+    {
+        __result = PhysicsSettings.TimeWarpLevels.Length - 2;
+    }
+}
